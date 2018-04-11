7515701ce3a35b08ea7382b5a67bac9ae5161be5
shubham <lanetteam.shubham10@gmail.com>
2018-04-11 10:57:29 +0530

    matarial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+///<reference path="../../node_modules/@angular/material/button/typings/button-module.d.ts"/>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCheckboxModule, MatDialogModule, MatDrawerContainer, MatIconModule, MatInputModule, MatMenuModule,
+  MatSelectModule, MatSidenavModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +18,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
